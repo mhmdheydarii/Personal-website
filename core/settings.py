30c60c919ff04f24047f41b7e37618423c952e58
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-09-07 02:07:43 -0700

    Update : CSRF

--- core/settings.py
+++ core/settings.py
@@ -139,7 +139,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://backend-x4q-mhmdhosin.runflare.cloud",
+    "https://backend-w81-mhmdhosin.runflare.cloud",
 ]
 
 SECURE_SSL_REDIRECT = False
