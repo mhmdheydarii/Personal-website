bd39cc60e71d50e33ad0f39ef55f2059d31c3cba
mhmdheydarii <heidarimohammadhosin@gmail.com>
2026-09-13 12:08:22 -0700

    settings update

--- core/settings.py
+++ core/settings.py
@@ -139,12 +139,12 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://mhmdhosin.ir",
+    "https://mhmdhosin.ir",
 ]
 
-# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
